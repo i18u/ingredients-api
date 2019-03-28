c7737bffbeaa909de40cd8622bd85003fe85c525
Ethan <xom.flareline@gmail.com>
2019-03-28 11:22:15 +0000

    Add: documentation Transport.Ingredient

--- Ingredients.Web/Models/Transport/Ingredient.cs
+++ Ingredients.Web/Models/Transport/Ingredient.cs
@@ -9,21 +9,35 @@ namespace Ingredients.Web.Models.Transport
     [DataContract]
     public class Ingredient
     {
+        /// <summary>
+        /// Unique ingredient ID
+        /// </summary>
         [DataMember(Name = "id")]
         public Guid Id { get; set; }
 
-
+        /// <summary>
+        /// Human-readable ingredient name
+        /// </summary>
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
-
+        /// <summary>
+        /// Short ingredient description
+        /// </summary>
         [DataMember(Name = "description")]
         public string Description { get; set; }
 
-
+        /// <summary>
+        /// Any tags / categorisation for this ingredient
+        /// </summary>
         [DataMember(Name = "tags")]
         public string[] Tags { get; set; }
 
+        /// <summary>
+        /// Convert a <see cref="Database.Ingredient"/> instance to a <see cref="Transport.Ingredient"/> model
+        /// </summary>
+        /// <param name="entity"><see cref="Database.Ingredient"/> to convert</param>
+        /// <returns>Converted <see cref="Transport.Ingredient"/> model</returns>
         public static Ingredient FromDatabase(Models.Database.Ingredient entity)
         {
             var ingr = new Ingredient();
