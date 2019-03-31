4a88181fe4997391d6f275a89aa3decbfcc85944
Aaron <hi@imaaronnicetomeetyou.me>
2019-03-31 15:33:52 +1100

    Revert "Merge branch 'test-2' into 'master'"
    This reverts commit fabab641550c4d6fb8b2b3759a0b790ea2e012a6, reversing
    changes made to de9529aec4eadeb3a176d80899340d09b428519c.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dotnet test ./Ingredients.Test/Ingredients.Test.csproj --logger="junit;LogFilePath=/test-results/results.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:Exclude=[Ingredients.Test]*
+dotnet test ./Ingredients.Test/Ingredients.Test.csproj --logger="junit;LogFilePath=/test-results/results.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=json
 
 echo "+--- Unit test results ---+"
 cat /test-results/results.xml
