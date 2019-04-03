0455c36b1e3e1d21355ecdc6ff4218a41a6fdec7
Eddie <crossbow711@gmail.com>
2019-04-03 11:36:53 +0000

    Revert "Made tests fail :)"
    This reverts commit 89e91fc41c74449e91b88e1665c34e0b9c631ae4.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 dotnet test ./Ingredients.Test/Ingredients.Test.csproj --logger="junit;LogFilePath=/test-results/results.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:Exclude=[Ingredients.Test]*
+OK=$?
 
 echo "+--- Unit test results ---+"
 cat /test-results/results.xml
 echo "\n"
 echo "\n"
+
+exit $OK
