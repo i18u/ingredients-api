d4dcd6075d97213bd1312d832b3c336160484055
FlareLine <xom.flareline@gmail.com>
2019-08-25 00:08:38 +1000

    Fix weird indentation

--- src/Ingredients.Web/Repositories/IngredientRepository.cs
+++ src/Ingredients.Web/Repositories/IngredientRepository.cs
@@ -3,15 +3,15 @@ using MongoDB.Driver;
 
 namespace Ingredients.Web.Repositories
 {
-    public class IngredientRepository : MongoRepository<Ingredient>
-    {
-	    private const string DatabaseKey = "cookbook";
+	public class IngredientRepository : MongoRepository<Ingredient>
+	{
+		private const string DatabaseKey = "cookbook";
 
-	    private const string CollectionKey = "ingredients";
+		private const string CollectionKey = "ingredients";
 
-        public IngredientRepository(IMongoClient client)
-	        : base(client.GetDatabase(DatabaseKey).GetCollection<Ingredient>(CollectionKey))
-        {
-        }
-    }
+		public IngredientRepository(IMongoClient client)
+			: base(client.GetDatabase(DatabaseKey).GetCollection<Ingredient>(CollectionKey))
+		{
+		}
+	}
 }
